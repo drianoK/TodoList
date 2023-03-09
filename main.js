e033c33abb408b2e8baf435de2c6d6cfd3f93dcd
drianoK <drianokvcs@gmail.com>
2023-03-09 20:50:24 +0100

    Define default const values

---
+++ main.js
@@ -0,0 +1,5 @@
+const input = document.getElementById("userinput");
+const addToInput = document.getElementById("addBtn");
+const todosAmount = document.getElementById("amount");
+const buttonComp = document.getElementById("completeBtn");
+const ul = document.querySelector("ul");
