dc7bf2bfca893af2e8728fa2f3b4c49905852451
drianoK <drianokvcs@gmail.com>
2023-03-09 21:33:26 +0100

    Remove todo-list classname from ul element in section.todos and added to li element - - Make the input field and the add button useable

--- main.js
+++ main.js
@@ -3,3 +3,47 @@ const addToInput = document.getElementById("addBtn");
 const todosAmount = document.getElementById("amount");
 const buttonComp = document.getElementById("completeBtn");
 const ul = document.querySelector("ul");
+
+const inputLength = () => value.input.length;
+
+const createListElement = () => {
+    const li = document.createElement("li");
+    li.classList.add("todo-list");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+
+}
+
+
+const addListAfterClick = (event) => {
+    event.prefentDefault();
+    if(inputLength() > 0 ) {
+        createTodo();
+    };
+}
+
+addToInput.addEventListener("click", createListElement);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
