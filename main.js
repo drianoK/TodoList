d14dccc9db7fb90bc9e057281905bb3c4bb890de
drianoK <drianokvcs@gmail.com>
2023-03-12 12:53:18 +0100

    Make h2 tag unvisible and p element visible in section.todos with hideHeading(), displayValues() when added a new todos - - Added new id to p element in section.todos - - Change styles to h1,h2

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
 const input = document.getElementById("userinput");
 const addToInput = document.getElementById("addBtn");
 const todosAmount = document.getElementById("amount");
-const buttonComp = document.getElementById("completeBtn");
+const buttonComp = document.querySelectorAll("#completeBtn");
+const redButton =document.getElementById("completeBtn");
 const ul = document.querySelector("ul");
+const addedUl = document.querySelectorAll("ul");
+const list = document.getElementsByTagName("li");
+const values = document.getElementsByClassName("values");
+
+
+
 
 const inputLength = () => input.value.length;
+const checkList = () => list.length;
+const checkAmount = () => values.length;
+
+
 
 const createListElement = () => {
+    //createList
     const li = document.createElement("li");
     li.classList.add("todo-list");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
     input.value = "";
+    hideHeading();
+    displayValues();
 
+    //createButton
     const button = document.createElement("button");
     button.setAttribute("id", "completeBtn");
     button.classList.add("red-btn");
     button.appendChild(document.createTextNode("Complete"));
-    li.appendChild(button);  
+    li.appendChild(button);
 }
-    
 
+
+
+const hideHeading = () => {
+    if(checkList() > 0 ) {
+        const emptyTodo = document.getElementById("empty");
+        emptyTodo.style.display = "none";
+    }   
+}   
+
+const displayValues = () => {
+    if(checkAmount() > 0) {
+        const text = document.getElementById("counter");
+        text.style.visibility = "visible";
+    }
+}
 
 const addListAfterClick = (event) => {
     event.preventDefault();
@@ -33,10 +62,24 @@ const addListAfterKeypress = (event) => {
         createListElement();
 }
 
+
 addToInput.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
 
 
+
+
+
+
+
+    
+
+    
+
+    
+
+
+   
 
 
 
